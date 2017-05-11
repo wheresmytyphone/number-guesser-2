359d1712bf1bf4be4df353d58a23bf97b6c95dd5
Tyler Garner <tylergarner@gmail.com>
2017-05-11 10:29:46 -0600

    clean up syntax/indentation, minor javascript adjustments

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var guessDisplay = document.querySelector('.guess-disp');
 var submit = document.querySelector('.submit');
 var clear = document.querySelector('.clear');
 var reset = document.querySelector('.reset');
-var go = document.querySelector('.go')
-var hint = document.querySelector('.hint')
+var go = document.querySelector('.go');
+var hint = document.querySelector('.hint');
 var correctNumber;
 
 function randomNumber() {
@@ -16,36 +16,37 @@ function randomNumber() {
 randomNumber()
 
 submit.addEventListener("click", function() {
-    guessDisplay.innerText = numGuess.value;
-    go.innerText = ""
-    console.log(numGuess.value)
-    compare()
-    numGuess.value = ""
+  guessDisplay.innerText = numGuess.value;
+  go.innerText = "";
+  console.log(numGuess.value);
+  compare();
+  numGuess.value = "";
 })
 
 reset.addEventListener('click', function() {
-    numGuess.value = ""
-    guessDisplay.innerText = ""
-    go.innerText = "Go ahead!"
-    hint.innerText = "Take a guess!"
-    randomNumber()
+  numGuess.value = "";
+  guessDisplay.innerText = "";
+  go.innerText = "Go ahead!";
+  hint.innerText = "Take a guess!";
+  randomNumber()
 })
 
 clear.addEventListener('click', function() {
-  numGuess.value = ""
+  numGuess.value = "";
+  guessDisplay.innerText = "";
 })
 function compare() {
-  var guessed = parseInt(numGuess.value)
+  var guessed = parseInt(numGuess.value);
   if (guessed < correctNumber) {
-      hint.innerText = "Too low!"
+    hint.innerText = "Too low!";
   } else if (guessed > 100) {
-      alert("I like your style but it's gotta be under a hundred")
-      guessDisplay.innerText = ""
+    alert("I like your style but it's gotta be under a hundred");
+    guessDisplay.innerText = "";
   } else if (guessed > correctNumber) {
-      hint.innerText = "Too high!"
+    hint.innerText = "Too high!";
   } else if (guessed === correctNumber) {
-      hint.innerText = "BOOM!"
+    hint.innerText = "BOOM!";
   } else if (NaN) {
-      alert('Numbers only, folks.')
+    alert("'Numbers only, folks.");
   }
 }
